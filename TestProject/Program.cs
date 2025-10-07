574ebf8f5c53b072eff280df26d6e8e96b305af3
Costel <costelnegura1705@gmail.com>
2025-10-07 11:52:40 +0200

    elseIf

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,18 +1,65 @@
-﻿Random dice = new Random();
+﻿// Random dice = new Random();
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
 
-int total = roll1 + roll2 + roll3;
+// int total = roll1 + roll2 + roll3;
 
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+// {
+//     Console.WriteLine("Hai fatto una doppia! hai un bonus di 2 punti!! ");
+//     total += 2;
+// }
+// if ((roll1 == roll2) && (roll2 == roll3)) 
+// {
+//     Console.WriteLine("Hai fatto triplette, hai un bonus di 6 punti!!");
+//     total += 6;
+// }
 
-if (total > 14)
-{
-    Console.WriteLine("You win");
-}
-if (total < 15)
-{
-    Console.WriteLine("Sorry, you lose.");
-}
+
+// if (total >= 15)
+// {
+//     Console.WriteLine("You win");
+//     Console.WriteLine(total);
+
+// }
+// if (total < 15)
+// {
+//     Console.WriteLine("Sorry, you lose.");
+//     Console.WriteLine(total);
+// }
+// // Espressione booleana possone essere : 
+// // 1 : un codice che ritorna true o false
+// // 2 : un metodo che restituisce valore true o false
+
+// string message = "La volpe saltela sopra il cane lento";
+// bool result = message.Contains("cane");
+// Console.WriteLine(result);
+// if (message.Contains("volpe"))// qua uso l'accesso ai membri(proprieta, metodo,campo,evento)
+// {
+//     Console.WriteLine("Cosa dice la volpe?");
+// }
+
+
+
+
+// Random random = new Random();
+// int daysUntilExpiration = random.Next(12);
+// int discountPercentage = 0;
+
+// if (daysUntilExpiration <= 10)
+// {
+//     Console.WriteLine("Your subscription will expire soon. Renew now!");
+// }
+// else if (daysUntilExpiration <= 5)
+// {
+//     Console.WriteLine($@"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save 10%!");
+// }else if (daysUntilExpiration == 1)
+// {
+//     Console.WriteLine($@"Your subscription expires within a day!\nRenew now and save 20%!");
+// }else if (daysUntilExpiration == 0)
+// {
+//     Console.WriteLine($@"Your subscription has expired.");
+// }
--- TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66697afc75bfaa7d9a8cb44955025ff57c2f44b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
